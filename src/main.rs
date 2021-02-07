56192fed79feffb70506b222fb2628166c259d89
Carsten König <carstenkoenig92@gmail.com>
2021-02-07 12:56:49 +0100

    adds https support

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+#[macro_use]
+extern crate lazy_static;
+
+use hyper_tls::{HttpsConnector};
 use warp::hyper::body::{Body, Bytes};
+use warp::hyper::client::connect::HttpConnector;
 use warp::hyper::{Client, Request};
 use warp::{
     http::{method::Method, HeaderMap, Response},
@@ -6,8 +11,31 @@ use warp::{
     Filter, Rejection,
 };
 
-fn log_request(method: &Method, path: &FullPath) {
-    println!("[{}] {}", method.as_str(), path.as_str())
+fn https_client() -> Client<HttpsConnector<HttpConnector>, Body> {
+    let mut tls_builder = hyper_tls::native_tls::TlsConnector::builder();
+    let tls_builder = tls_builder.danger_accept_invalid_certs(true);
+    let tls_builder = tls_builder.danger_accept_invalid_hostnames(true);
+
+    let tls = tls_builder.build().unwrap();
+
+    let mut http = HttpConnector::new();
+    http.enforce_http(false);
+    let https = HttpsConnector::from((http, tls.into()));
+
+    Client::builder().build(https)
+}
+
+lazy_static! {
+    static ref STATIC_CLIENT: Client<HttpsConnector<HttpConnector>, Body> = https_client();
+}
+
+fn log_start_request(method: &Method, path: &FullPath) {
+    let method_str = format!("[{}]", method.as_str());
+    println!("{:6} {}", method_str, path.as_str())
+}
+
+fn log_done_request(status: warp::hyper::http::StatusCode) {
+    println!(" => {}", status)
 }
 
 async fn proxy_request(
@@ -16,13 +44,13 @@ async fn proxy_request(
     headers: HeaderMap,
     body: Bytes,
 ) -> Result<Response<Body>, Rejection> {
-    log_request(&method, &path);
+    log_start_request(&method, &path);
 
-    let client = Client::new();
     let request = build_request(&method, &path, &headers, body);
-    let response = client.request(request).await.unwrap();
+    let response = STATIC_CLIENT.request(request).await.unwrap();
     let response_status = response.status();
     let response_body = response.into_body();
+    log_done_request(response_status);
 
     Ok(Response::builder()
         .status(response_status)
@@ -36,7 +64,7 @@ fn build_request(
     headers: &HeaderMap,
     body: Bytes,
 ) -> Request<Body> {
-    let location = format!("http://localhost:9200{}", path.as_str());
+    let location = format!("https://localhost{}", path.as_str());
 
     let mut request = Request::builder().method(method.as_str()).uri(location);
 
